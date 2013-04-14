872ef2efd493a53d45054ce9181193d0d8881403
Julia Oh <juliahhh.oh@gmail.com>
2013-04-13 22:37:39 -0700

    q2 done

--- bustersAgents.py
+++ bustersAgents.py
@@ -152,4 +152,18 @@ class GreedyBustersAgent(BustersAgent):
                                             in enumerate(self.ghostBeliefs)
                                             if livingGhosts[i+1]]
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        localMax = []
+        for belief in livingGhostPositionDistributions:
+            localMax.append(belief.argMax())
+        goalCoordinate, goalProbability = None, 0
+        for index, coordinate in enumerate(localMax):
+            if livingGhostPositionDistributions[index][coordinate] >= goalProbability:
+                goalCoordinate, goalProbability = coordinate, livingGhostPositionDistributions[index][coordinate]
+
+        temp = []
+        for action in legal:
+            nextLocation = Actions.getSuccessor(pacmanPosition, action)
+            temp.append((self.distancer.getDistance(nextLocation, goalCoordinate), action))
+        return min(temp)[1]
+
+        # util.raiseNotDefined()
